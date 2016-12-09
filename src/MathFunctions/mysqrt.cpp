4c76ea4851a01ab9f52882befbd748429e03536b
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2016-12-09 16:00:30 -0500

    CMake Tutorial -- Step 4, Introspection (finding if certain symbols exist)

--- src/MathFunctions/mysqrt.cpp
+++ src/MathFunctions/mysqrt.cpp
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
+#include "SandboxConfig.h"
+
 float mysqrt(float f) {
     printf("Using mysqrt.\n");
-    
-    float ret = sqrt(f);
+
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+    float ret = exp(log(f) * 0.5);
+#else  // defined(HAVE_LOG) && defined(HAVE_EXP)
+#error "NO LOG AND/OR EXP FUNCTION TO USE"
+#endif // defined(HAVE_LOG) && defined(HAVE_EXP)
     
     if (isnan(ret)) {
         ret = 0;
