de223c06974ddb7bdb0db8e8c0c0c97ad12b7564
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-09-03 12:25:35 -0400

    Moved template out of the app.component file.

--- angular/angular_tour_of_heroes/src/app/app.component.ts
+++ angular/angular_tour_of_heroes/src/app/app.component.ts
@@ -6,13 +6,6 @@ import { HeroService } from './services/hero.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-    <nav>
-      <a routerLink="/">Dashboard</a>
-      <a routerLink="/heroes">Heroes</a>
-    </nav>
-    <router-outlet></router-outlet>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
