7850ddc6b035d3cc4c8c0667623e04c518a3bbde
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-09-05 21:31:43 -0400

    Changed CanvasDottedLogo selector.

--- Angular/svg-canvas-tutorial/src/app/canvas/components/canvas-dotted-logo/canvas-dotted-logo.component.ts
+++ Angular/svg-canvas-tutorial/src/app/canvas/components/canvas-dotted-logo/canvas-dotted-logo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-canvas-dotted-logo',
+  selector: 'canvas-dotted-logo',
   templateUrl: './canvas-dotted-logo.component.html',
   styleUrls: ['./canvas-dotted-logo.component.css']
 })
