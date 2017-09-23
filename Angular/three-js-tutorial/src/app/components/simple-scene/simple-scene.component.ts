b282ed84f2a5f5f994944a7cff2361fdcddbd50e
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-09-23 19:16:43 -0400

    Created stuff for the ThreeJS Scene. Not used currently.

--- Angular/three-js-tutorial/src/app/components/simple-scene/simple-scene.component.ts
+++ Angular/three-js-tutorial/src/app/components/simple-scene/simple-scene.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
-  selector: 'app-simple-scene',
-  templateUrl: './simple-scene.component.html',
-  styleUrls: ['./simple-scene.component.css']
+    selector: 'app-simple-scene',
+    templateUrl: './simple-scene.component.html',
+    styleUrls: ['./simple-scene.component.css']
 })
 export class SimpleSceneComponent implements OnInit {
 
-  constructor() { }
+    public readonly WIDTH  = 400;
+    public readonly HEIGHT = 300;
 
-  ngOnInit() {
-  }
+    public readonly VIEW_ANGLE = 45.0;
+    public readonly ASPECT = this.WIDTH / this.HEIGHT;
+
+    public readonly NEAR = 0.1;
+    public readonly FAR  = 10000.0;
+
+    private readonly RENDERER: THREE.WebGLRenderer;
+    private readonly CAMERA: THREE.PerspectiveCamera;
+    private readonly SCENE: THREE.Scene;
+
+    constructor() {
+        this.RENDERER = new THREE.WebGLRenderer();
+        this.CAMERA = new THREE.PerspectiveCamera(
+            this.VIEW_ANGLE,
+            this.ASPECT,
+            this.NEAR,
+            this.FAR
+        );
+        this.SCENE = new THREE.Scene();
+    }
+
+    ngOnInit() {
+    }
 
 }
