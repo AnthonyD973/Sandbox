2cc172385b418fad3a81692f89da783703f91a5a
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-01-03 12:46:51 -0500

    Added source of BasicFragmentShader.

--- WebGL/mdnTutorial/src/app/src/shaders/fragment-shaders/basic-fragment-shader.ts
+++ WebGL/mdnTutorial/src/app/src/shaders/fragment-shaders/basic-fragment-shader.ts
@@ -2,4 +2,10 @@ import { AbstractFragmentShader } from "./abstract-fragment-shader";
 
 export class BasicFragmentShader extends AbstractFragmentShader {
 
+    public static readonly SOURCE = `
+        void main() {
+            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+        }
+    `;
+
 }
