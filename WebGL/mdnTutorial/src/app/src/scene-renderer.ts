ef0c26c2145b5508810dc81335da195089a37e3d
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-01-02 21:32:36 -0500

    Created the rendering methods of SceneRenderer.

--- WebGL/mdnTutorial/src/app/src/scene-renderer.ts
+++ WebGL/mdnTutorial/src/app/src/scene-renderer.ts
@@ -6,6 +6,34 @@ export class SceneRenderer {
 
     private static readonly INSTANCE = new SceneRenderer();
 
+    private isRendering = false;
+
     protected constructor() { }
+
+    public startRendering(): void {
+        if (!this.isRendering) {
+            this.render();
+        }
+        else {
+            throw new Error(`Already started rendering.`);
+        }
+    }
+
+    public stopRendering(): void {
+        if (this.isRendering) {
+            this.isRendering = false;
+        }
+        else {
+            throw new Error(`Not currently rendering.`);
+        }
+    }
+
+    protected render(): void {
+        requestAnimationFrame(() => {
+            if (this.isRendering) {
+                this.render();
+            }
+        });
+    }
 
 }
