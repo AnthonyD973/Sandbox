37a63f8437a6562ea8acc5da8e94d14b8cb678b9
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-01-03 12:24:07 -0500

    Added the vertex shader's source.

--- WebGL/mdnTutorial/src/app/src/shaders/vertex-shader.ts
+++ WebGL/mdnTutorial/src/app/src/shaders/vertex-shader.ts
@@ -1,3 +1,16 @@
+import { SceneRenderer } from "../scene-renderer";
+
 export class VertexShader {
-    
+
+    public static readonly SOURCE = `
+        attribute vec4 vertex;
+
+        uniform mat4 viewModelMatrix;
+        uniform mat4 projectionMatrix;
+
+        void main() {
+            gl_Position = projectionMatrix * viewModelMatrix * vertex;
+        }
+    `;
+
 }
