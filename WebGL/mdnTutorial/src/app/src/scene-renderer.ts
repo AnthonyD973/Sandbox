d1fec62a5d82ae0d1774bab85bf1287bcef99d85
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-01-02 21:23:43 -0500

    Made SceneRenderer a singleton class.

--- WebGL/mdnTutorial/src/app/src/scene-renderer.ts
+++ WebGL/mdnTutorial/src/app/src/scene-renderer.ts
@@ -1,3 +1,11 @@
 export class SceneRenderer {
 
+    public static getInstance(): SceneRenderer {
+        return SceneRenderer.INSTANCE;
+    }
+
+    private static readonly INSTANCE = new SceneRenderer();
+
+    protected constructor() { }
+
 }
