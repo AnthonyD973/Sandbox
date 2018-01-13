68fba8c32ebfdce3a29d6aca133d686b5b4d2279
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-01-12 22:12:24 -0500

    Impleemented contructor of AbstractShader.

--- WebGL/MdnTutorial/src/app/webgl/shaders/abstract-shader.ts
+++ WebGL/MdnTutorial/src/app/webgl/shaders/abstract-shader.ts
@@ -1,3 +1,19 @@
 export abstract class AbstractShader {
 
+    private shader: WebGLShader;
+
+    constructor(gl: WebGLRenderingContext, shaderType: number, source: string) {
+        this.shader = gl.createShader(shaderType);
+        gl.shaderSource(this.shader, source);
+        gl.compileShader(this.shader);
+
+        const COMPILED_SUCCESSFULLY = gl.getShaderParameter(this.shader, gl.COMPILE_STATUS);
+        if (!COMPILED_SUCCESSFULLY) {
+            const ERROR_MESSAGE = 'An error occurred while compiling a WebGL shader. We apologize for the inconveniance.\nShader log: ' +
+                gl.getShaderInfoLog(this.shader);
+            alert(ERROR_MESSAGE);
+            throw new Error(ERROR_MESSAGE);
+        }
+    }
+
 }
