bf68cec6aed541b793208f3cd7f195839de9f704
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-01-19 14:45:57 -0500

    Added inputs and outputs to ShaderSourceData.

--- WebGL/MdnTutorial/src/app/webgl/shaders/source/shader-source-data.ts
+++ WebGL/MdnTutorial/src/app/webgl/shaders/source/shader-source-data.ts
@@ -1,3 +1,9 @@
+import { AbstractShaderInput } from './abstract-shader-input';
+import { AbstractShaderOutput } from './abstract-shader-output';
+
 export interface ShaderSourceData {
+
+    inputs: Map<string, AbstractShaderInput>;
+    outputs: Map<string, AbstractShaderOutput>;
 
 }
