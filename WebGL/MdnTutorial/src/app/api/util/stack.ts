198a5624083318a7cf74d8ab9f3f21c36db77105
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-01-19 15:48:27 -0500

    Empty implementation of Stack's methods.

--- WebGL/MdnTutorial/src/app/api/util/stack.ts
+++ WebGL/MdnTutorial/src/app/api/util/stack.ts
@@ -1,3 +1,14 @@
 export class Stack<T> {
 
+    public push(value: T): void {
+    }
+
+    public pop(): T {
+        return null;
+    }
+
+    public get length(): number {
+        return 1;
+    }
+
 }
