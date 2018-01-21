ec9bb9e9653778d1871f24619f98e0a81babf41b
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-01-20 20:22:45 -0500

    Created some methods on ShaderLocalScope.

--- WebGL/MdnTutorial/src/app/api/shaders/source/scope/shader-local-scope.ts
+++ WebGL/MdnTutorial/src/app/api/shaders/source/scope/shader-local-scope.ts
@@ -2,4 +2,9 @@ import { ShaderScope } from './shader-scope';
 
 export abstract class ShaderLocalScope extends ShaderScope {
 
+    public abstract startIf(): ShaderLocalScope;
+    public abstract startElseIf(): ShaderLocalScope;
+    public abstract startElse(): ShaderLocalScope;
+    public abstract endIf(): ShaderLocalScope;
+
 }
