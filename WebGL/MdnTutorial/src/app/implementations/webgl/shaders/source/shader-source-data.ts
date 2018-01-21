abe8dc2bf51776c82aa5583f06504593a14291aa
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-01-20 19:52:54 -0500

    Moved the ShaderInput and ShaderOutput classes, and renamed them so.

--- WebGL/MdnTutorial/src/app/implementations/webgl/shaders/source/shader-source-data.ts
+++ WebGL/MdnTutorial/src/app/implementations/webgl/shaders/source/shader-source-data.ts
@@ -1,9 +1,9 @@
-import { AbstractShaderInput } from '../../../../api/shaders/source/abstract-shader-input';
-import { AbstractShaderOutput } from '../../../../api/shaders/source/abstract-shader-output';
+import { ShaderInput } from '../../../../api/shaders/source/scope/shader-input';
+import { ShaderOutput } from '../../../../api/shaders/source/scope/shader-output';
 
 export interface ShaderSourceData {
 
-    inputs: Map<string, AbstractShaderInput>;
-    outputs: Map<string, AbstractShaderOutput>;
+    inputs: Map<string, ShaderInput>;
+    outputs: Map<string, ShaderOutput>;
 
 }
