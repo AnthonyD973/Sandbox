c69ab3a5c5bb27b52ce6a1525e0ac965c0f6272e
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-01-20 20:19:14 -0500

    Added some methods in ShaderGlobalScope.

--- WebGL/MdnTutorial/src/app/api/shaders/source/scope/shader-global-scope.ts
+++ WebGL/MdnTutorial/src/app/api/shaders/source/scope/shader-global-scope.ts
@@ -1,5 +1,14 @@
 import { ShaderScope } from './shader-scope';
+import { ShaderInput } from './shader-input';
+import { ShaderOutput } from './shader-output';
+import { ShaderFunction } from './shader-function';
 
 export abstract class ShaderGlobalScope extends ShaderScope {
+
+    public abstract startFunction(): ShaderFunction;
+    public abstract endFunction(): void;
+
+    public abstract createInput(): ShaderInput;
+    public abstract createOutput(): ShaderOutput;
 
 }
