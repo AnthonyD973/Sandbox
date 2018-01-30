75396e2cbf0a6307dee258f3db8a7dd228fa4032
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-01-29 21:11:39 -0500

    Added type attribute to ShaderExpression.

--- WebGL/MdnTutorial/src/app/api/shaders/source/expression/shader-expression.ts
+++ WebGL/MdnTutorial/src/app/api/shaders/source/expression/shader-expression.ts
@@ -1,6 +1,9 @@
 import { ShaderParsable } from '../shader-parsable';
+import { ShaderExpressionType } from './shader-expression-type';
 
 export abstract class ShaderExpression implements ShaderParsable {
+
+    public readonly type: ShaderExpressionType;
 
     public abstract parse(): any;
 
