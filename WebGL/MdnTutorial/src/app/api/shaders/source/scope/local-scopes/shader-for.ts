db22e09a8ea33376b250ca2a2b30943763fadab7
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-02-03 17:55:53 -0500

    Updated ShaderFor and ShaderWhile according to the design.

--- WebGL/MdnTutorial/src/app/api/shaders/source/scope/local-scopes/shader-for.ts
+++ WebGL/MdnTutorial/src/app/api/shaders/source/scope/local-scopes/shader-for.ts
@@ -1,5 +1,17 @@
 import { ShaderLocalScope } from '../shader-local-scope';
+import { ShaderExpression } from '../../expression/shader-expression';
 
 export abstract class ShaderFor extends ShaderLocalScope {
+
+    public readonly initExpr: ShaderExpression;
+    public readonly condExpr: ShaderExpression;
+    public readonly loopExpr: ShaderExpression;
+
+    constructor(initExpr: ShaderExpression, condExpr: ShaderExpression, loopExpr: ShaderExpression) {
+        super();
+        this.initExpr = initExpr;
+        this.condExpr = condExpr;
+        this.loopExpr = loopExpr;
+    }
 
 }
--- WebGL/MdnTutorial/src/app/api/shaders/source/scope/local-scopes/shader-while.ts
+++ WebGL/MdnTutorial/src/app/api/shaders/source/scope/local-scopes/shader-while.ts
@@ -1,5 +1,13 @@
 import { ShaderLocalScope } from '../shader-local-scope';
+import { ShaderExpression } from '../../expression/shader-expression';
 
 export abstract class ShaderWhile extends ShaderLocalScope {
+
+    public condExpr: ShaderExpression;
+
+    constructor(condExpr: ShaderExpression) {
+        super();
+        this.condExpr = condExpr;
+    }
 
 }
