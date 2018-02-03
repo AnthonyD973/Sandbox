80965570b73c6b7e7c2db271d755ce166ca0282e
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-02-02 20:54:20 -0500

    Fixed import.

--- WebGL/MdnTutorial/src/app/api/shaders/source/scope/shader-global-scope.ts
+++ WebGL/MdnTutorial/src/app/api/shaders/source/scope/shader-global-scope.ts
@@ -1,6 +1,6 @@
 import { ShaderScope } from './shader-scope';
-import { ShaderInput } from '../expression/values/shader-input';
-import { ShaderOutput } from '../expression/values/shader-output';
+import { ShaderInput } from '../expression/lvalues/shader-input';
+import { ShaderOutput } from '../expression/lvalues/shader-output';
 import { ShaderFunction } from './local-scopes/shader-function';
 
 export abstract class ShaderGlobalScope extends ShaderScope {
