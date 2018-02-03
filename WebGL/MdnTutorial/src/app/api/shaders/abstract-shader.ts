3563e2d348f74f47bb46ae15c27d776d01096be3
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-02-02 20:49:05 -0500

    Created class AbstractShader.

--- WebGL/MdnTutorial/src/app/api/shaders/abstract-shader.ts
+++ WebGL/MdnTutorial/src/app/api/shaders/abstract-shader.ts
@@ -1,7 +1,11 @@
+<<<<<<< f8ec0d661b8e25ea77e2ba888a439db7d2d70c4c
 import { ShaderParsable } from './source/shader-parsable';
 
 export abstract class AbstractShader implements ShaderParsable {
 
     public abstract parse(): any;
+=======
+export abstract class AbstractShader {
+>>>>>>> Created class AbstractShader.
 
 }
