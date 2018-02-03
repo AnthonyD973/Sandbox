f1630cfe4113b0a33ba5091b07ea947aff4c348a
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-02-03 17:49:29 -0500

    Updated ShaderFunction according to design.

--- WebGL/MdnTutorial/src/app/api/shaders/source/scope/local-scopes/shader-function.ts
+++ WebGL/MdnTutorial/src/app/api/shaders/source/scope/local-scopes/shader-function.ts
@@ -1,5 +1,15 @@
 import { ShaderLocalScope } from '../shader-local-scope';
+import { ShaderFunctionSignature } from '../../expression/types/shader-function-signature';
 
 export abstract class ShaderFunction extends ShaderLocalScope {
+
+    public readonly name: string;
+    public readonly signature: ShaderFunctionSignature;
+
+    constructor(name: string, signature: ShaderFunctionSignature) {
+        super();
+        this.name = name;
+        this.signature = signature;
+    }
 
 }
