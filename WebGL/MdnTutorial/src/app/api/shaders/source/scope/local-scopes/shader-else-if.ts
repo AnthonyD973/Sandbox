434903daf3fb4f130f0734239e7f6757a46e2981
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-02-03 18:03:42 -0500

    Added the condition expression inside ShaderIf and ShaderElseIf.

--- WebGL/MdnTutorial/src/app/api/shaders/source/scope/local-scopes/shader-else-if.ts
+++ WebGL/MdnTutorial/src/app/api/shaders/source/scope/local-scopes/shader-else-if.ts
@@ -1,6 +1,12 @@
 import { ShaderLocalScope } from '../shader-local-scope';
 import { ShaderIf } from './shader-if';
+import { ShaderScope } from '../shader-scope';
+import { ShaderExpression } from '../../expression/shader-expression';
 
 export abstract class ShaderElseIf extends ShaderIf {
+
+    constructor(parent: ShaderScope, condExpr: ShaderExpression) {
+        super(parent, condExpr);
+    }
 
 }
--- WebGL/MdnTutorial/src/app/api/shaders/source/scope/local-scopes/shader-if.ts
+++ WebGL/MdnTutorial/src/app/api/shaders/source/scope/local-scopes/shader-if.ts
@@ -1,11 +1,19 @@
 import { ShaderLocalScope } from '../shader-local-scope';
 import { ShaderElseIf } from './shader-else-if';
 import { ShaderElse } from './shader-else';
+import { ShaderExpression } from '../../expression/shader-expression';
+import { ShaderScope } from '../shader-scope';
 
 export abstract class ShaderIf extends ShaderLocalScope {
 
     private next: ShaderElseIf | ShaderElse;
     private hasNext = false;
+    private condExpr: ShaderExpression;
+
+    constructor(parent: ShaderScope, condExpr: ShaderExpression) {
+        super(parent);
+        this.condExpr = condExpr;
+    }
 
     public abstract elseIf(): ShaderElseIf;
     public abstract else(): ShaderElse;
