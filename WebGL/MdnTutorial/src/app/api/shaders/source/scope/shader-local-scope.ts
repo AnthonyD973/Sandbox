216cee4576012d5c6479b83994bd84192d7e0116
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-02-03 17:28:45 -0500

    Made ShaderLocalScope match the design.

--- WebGL/MdnTutorial/src/app/api/shaders/source/scope/shader-local-scope.ts
+++ WebGL/MdnTutorial/src/app/api/shaders/source/scope/shader-local-scope.ts
@@ -1,10 +1,12 @@
 import { ShaderScope } from './shader-scope';
+import { ShaderFor } from './local-scopes/shader-for';
+import { ShaderWhile } from './local-scopes/shader-while';
 
 export abstract class ShaderLocalScope extends ShaderScope {
 
-    public abstract startIf(): ShaderLocalScope;
-    public abstract startElseIf(): ShaderLocalScope;
-    public abstract startElse(): ShaderLocalScope;
-    public abstract endIf(): ShaderLocalScope;
+    public abstract end(): ShaderLocalScope;
+    public abstract if(): ShaderLocalScope;
+    public abstract for(): ShaderFor;
+    public abstract while(): ShaderWhile;
 
 }
