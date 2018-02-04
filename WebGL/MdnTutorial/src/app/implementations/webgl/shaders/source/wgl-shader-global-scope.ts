93fb096ca16af9607fb1321d41fcc6a17ba4db7b
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-02-03 19:20:24 -0500

    Created class WglShaderGlobalScope

---
+++ WebGL/MdnTutorial/src/app/implementations/webgl/shaders/source/wgl-shader-global-scope.ts
@@ -0,0 +1,21 @@
+import { ShaderGlobalScope } from '../../../../api/shaders/source/scope/shader-global-scope';
+
+export class WglShaderGlobalScope extends ShaderGlobalScope {
+
+    public parse(): string {
+        return null;
+    }
+
+    public createFunction(): any {
+
+    }
+
+    public createInput(): any {
+
+    }
+
+    public createOutput(): any {
+
+    }
+
+}
