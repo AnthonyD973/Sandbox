3f71532498e5645d45ea2342e8e653ea8653203d
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-02-03 19:12:01 -0500

    Created Shader classes for WebGL implementation.

---
+++ WebGL/MdnTutorial/src/app/implementations/webgl/shaders/wgl-vertex-shader.ts
@@ -0,0 +1,9 @@
+import { VertexShader } from '../../../api/shaders/vertex-shader';
+
+export class WglVertexShader extends VertexShader {
+
+    public parse(): string {
+        return null;
+    }
+
+}
