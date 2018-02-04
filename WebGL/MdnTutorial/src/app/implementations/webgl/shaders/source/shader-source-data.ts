7a8addf2112c4d448df833e0625ea16451869a1f
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-02-03 19:02:38 -0500

    Removed yet another unused class.

--- WebGL/MdnTutorial/src/app/implementations/webgl/shaders/source/shader-source-data.ts
+++
@@ -1,9 +0,0 @@
-import { ShaderInput } from '../../../../api/shaders/source/expression/values/shader-input';
-import { ShaderOutput } from '../../../../api/shaders/source/expression/values/shader-output';
-
-export interface ShaderSourceData {
-
-    inputs: Map<string, ShaderInput>;
-    outputs: Map<string, ShaderOutput>;
-
-}
