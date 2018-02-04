487986da3d581f7a25265c88fb06cfff58ff3a18
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-02-03 19:18:58 -0500

    Created class WglShaderLocalScope.

---
+++ WebGL/MdnTutorial/src/app/implementations/webgl/shaders/source/wgl-shader-local-scope.ts
@@ -0,0 +1,25 @@
+import { ShaderLocalScope } from '../../../../api/shaders/source/scope/shader-local-scope';
+
+export class WglShaderLocalScope extends ShaderLocalScope {
+
+    public parse(): string {
+        return null;
+    }
+
+    public end(): void {
+
+    }
+
+    public if (): any {
+
+    }
+
+    public for(): any {
+
+    }
+
+    public while(): any {
+
+    }
+
+}
