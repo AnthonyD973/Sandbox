25e370392a616985dca66434cc1a4792235db31e
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-02-03 19:22:29 -0500

    Fixed missing import.

--- WebGL/MdnTutorial/src/app/api/shaders/source/scope/local-scopes/shader-while.ts
+++ WebGL/MdnTutorial/src/app/api/shaders/source/scope/local-scopes/shader-while.ts
@@ -1,5 +1,6 @@
 import { ShaderLocalScope } from '../shader-local-scope';
 import { ShaderExpression } from '../../expression/shader-expression';
+import { ShaderScope } from '../shader-scope';
 
 export abstract class ShaderWhile extends ShaderLocalScope {
 
