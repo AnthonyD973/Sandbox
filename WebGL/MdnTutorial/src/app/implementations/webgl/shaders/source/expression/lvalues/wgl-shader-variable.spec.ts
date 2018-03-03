588fee6f0d7e8fe55daf865c5135c983646a8964
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-03-02 22:11:46 -0500

    Created empty test set for WglShaderVariable.

---
+++ WebGL/MdnTutorial/src/app/implementations/webgl/shaders/source/expression/lvalues/wgl-shader-variable.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { WglShaderVariable } from './wgl-shader-variable';
+import { WglShaderAssignment } from '../operators/binary/wgl-shader-assignment';
+
+describe('WglShaderVariable', () => {
+
+    beforeEach(() => TestBed.configureTestingModule({
+    }));
+
+    beforeEach(inject([], () => {
+    }));
+
+    describe('parse', () => {
+    });
+
+    describe('assign', () => {
+    });
+
+});
