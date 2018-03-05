a478f31e8f8e95f07577cf059d832eedac505abc
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-03-05 16:08:14 -0500

    Added interface ShaderMatrixDims.

--- WebGL/MdnTutorial/src/app/api/shaders/source/expression/types/shader-matrix-type.ts
+++ WebGL/MdnTutorial/src/app/api/shaders/source/expression/types/shader-matrix-type.ts
@@ -1,5 +1,10 @@
 import { ShaderExpressionType } from '../shader-expression-type';
 
+export interface ShaderMatrixDims {
+    rows: number;
+    cols: number;
+}
+
 export interface ShaderMatrixType extends ShaderExpressionType {
 
 }
