31ee198e915d95d98f1d2582c02bbae7b7bdba0a
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-03-04 23:05:05 -0500

    Implemented class WglShaderVariable.

--- WebGL/MdnTutorial/src/app/implementations/webgl/shaders/source/expression/lvalues/wgl-shader-variable.ts
+++ WebGL/MdnTutorial/src/app/implementations/webgl/shaders/source/expression/lvalues/wgl-shader-variable.ts
@@ -2,19 +2,27 @@ import { ShaderVariable } from '../../../../../../api/shaders/source/expression/
 import { ShaderExpressionType } from '../../../../../../api/shaders/source/expression/shader-expression-type';
 import { ShaderExpression } from '../../../../../../api/shaders/source/expression/shader-expression';
 import { WglShaderAssignment } from '../operators/binary/wgl-shader-assignment';
+import { WglIdentifierRegex } from '../wgl-identifier-regex';
 
 export class WglShaderVariable implements ShaderVariable {
+
+    protected static readonly identifierRegex = new WglIdentifierRegex();
 
     public readonly name: string;
     public readonly type: ShaderExpressionType;
 
     constructor(name: string, type: ShaderExpressionType) {
-        this.name = name;
-        this.type = type;
+        if (name !== null && WglShaderVariable.identifierRegex.test(name)) {
+            this.name = name;
+            this.type = type;
+        }
+        else {
+            throw new Error(`Name "${name}" is an invalid identifier for a variable`);
+        }
     }
 
     public parse(): string {
-        return null;
+        return (this.type.parse() as String) + ' ' + this.name + ';';
     }
 
 }
