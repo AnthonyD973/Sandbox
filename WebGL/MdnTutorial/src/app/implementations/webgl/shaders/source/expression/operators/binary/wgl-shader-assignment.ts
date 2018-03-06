b074ea518a2bfab57e43bb382b93fe0b6c718661
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-03-06 08:21:20 -0500

    Added an 'assignee' field in WglShaderAssignment.

--- WebGL/MdnTutorial/src/app/implementations/webgl/shaders/source/expression/operators/binary/wgl-shader-assignment.ts
+++ WebGL/MdnTutorial/src/app/implementations/webgl/shaders/source/expression/operators/binary/wgl-shader-assignment.ts
@@ -1,15 +1,18 @@
 import { ShaderAssignment } from '../../../../../../../api/shaders/source/expression/operators/binary/shader-assignment';
 import { ShaderExpressionType } from '../../../../../../../api/shaders/source/expression/shader-expression-type';
 import { ShaderExpression } from '../../../../../../../api/shaders/source/expression/shader-expression';
+import { WglShaderVariable } from '../../lvalues/wgl-shader-variable';
 
 export class WglShaderAssignment implements ShaderAssignment {
 
     public readonly type: ShaderExpressionType;
+    public readonly assignee: WglShaderVariable;
     public readonly assignedExpression: ShaderExpression;
 
-    constructor(assignedExpression: ShaderExpression) {
-        this.assignedExpression = assignedExpression;
+    constructor(assignee: WglShaderVariable, assignedExpression: ShaderExpression) {
         this.type = assignedExpression.type;
+        this.assignee = assignee;
+        this.assignedExpression = assignedExpression;
     }
 
     public parse(): string {
