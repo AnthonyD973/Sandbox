326cb04091796167d92957f9b117fb96b3f2ad09
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-03-05 19:12:26 -0500

    Added mat and vec dimensions in WglShaderConfig.

--- WebGL/MdnTutorial/src/app/implementations/webgl/shaders/util/wgl-shader-config.ts
+++ WebGL/MdnTutorial/src/app/implementations/webgl/shaders/util/wgl-shader-config.ts
@@ -1,3 +1,13 @@
 export class WglShaderConfig {
 
+    /**
+     * Minimum dimension for vectors and matrices.
+     */
+    public static readonly DIM_MIN = 2;
+
+    /**
+     * Maximum dimension for vectors and matrices.
+     */
+    public static readonly DIM_MAX = 4;
+
 }
