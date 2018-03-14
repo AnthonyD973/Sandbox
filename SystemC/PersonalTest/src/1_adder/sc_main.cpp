9fc2785386d35e59d2266fc078fb19122d48e937
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-03-14 13:24:58 -0400

    Added seed setting in sc_main.

--- SystemC/PersonalTest/src/1_adder/sc_main.cpp
+++ SystemC/PersonalTest/src/1_adder/sc_main.cpp
@@ -1,11 +1,16 @@
 #include <systemc.h>
 #include <inttypes.h>
+#include <ctime>
+#include <cstdlib>
 
+#include "top.h"
 #include "adder.h"
 
 constexpr uint8_t ADDER_WIDTH = 4;
 
 int sc_main(int argc, char** argv) {
+    srand(time(NULL));
+
     sc_buffer< sc_dt::sc_uint<ADDER_WIDTH> > a;
     sc_buffer< sc_dt::sc_uint<ADDER_WIDTH> > b;
     sc_buffer< sc_dt::sc_uint<ADDER_WIDTH + 1> > sum;
