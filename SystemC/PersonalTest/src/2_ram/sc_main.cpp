ea3cfa1db5e9588f4b52e1505fd8bbd0322456c0
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-03-14 17:23:32 -0400

    Connected buffers to the top instance.

--- SystemC/PersonalTest/src/2_ram/sc_main.cpp
+++ SystemC/PersonalTest/src/2_ram/sc_main.cpp
@@ -2,7 +2,20 @@
 
 #include "top.h"
 
+constexpr int ADDR_SIZE = 16;
+constexpr int WORD_SIZE = 16;
+constexpr int MEM_SIZE  = 65536; 
+
 int sc_main(int argc, char** argv) {
-    Top<16, 16> top("t1");
+    sc_buffer< sc_dt::sc_uint<ADDR_SIZE> > addr;
+    sc_buffer< sc_dt::sc_uint<WORD_SIZE> > val;
+    sc_buffer<bool> done;
+
+    Top<ADDR_SIZE, WORD_SIZE, MEM_SIZE> top("t1");
+    top.m_addr(addr);
+    top.m_val(val);
+    top.m_done(done);
+    
+    sc_start(200, SC_NS);
     return 0;
 }
