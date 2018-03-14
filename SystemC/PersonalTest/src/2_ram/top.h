13c2dddb137b64de7e5717f54498ee7aa1746433
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-03-14 17:23:09 -0400

    Made Top stimulate the ports.

--- SystemC/PersonalTest/src/2_ram/top.h
+++ SystemC/PersonalTest/src/2_ram/top.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <cinttypes>
+#include <cstdlib>
 
 #include "sc_uint_hash.h"
 
@@ -10,18 +11,21 @@ SC_MODULE(Top) {
 
 public:
 
-    sc_core::sc_in< sc_dt::sc_uint<WORD_SIZE> > m_val;
+    sc_core::sc_in< sc_dt::sc_uint<WORD_SIZE> >                 m_val;
+    sc_core::sc_in<bool>                                        m_done;
+    sc_core::sc_out< sc_dt::sc_uint<ADDR_SIZE> >                m_addr;
 
     SC_CTOR(Top);
 
     void stimulateRam();
+    void checkValue() const;
 
 private:
 
     std::unordered_map<
         sc_dt::sc_uint<ADDR_SIZE>,
         sc_dt::sc_uint<WORD_SIZE>
-    > m_cachedRam;
+    >                                                           m_cachedRam;
 
 };
 
@@ -35,7 +39,35 @@ Top<ADDR_SIZE, WORD_SIZE, MEM_SIZE>::Top(::sc_core::sc_module_name name) {
 
 }
 
+
 template<int ADDR_SIZE, int WORD_SIZE, int MEM_SIZE>
 void Top<ADDR_SIZE, WORD_SIZE, MEM_SIZE>::stimulateRam() {
-    
+    while (true) {
+        int addr = rand() % MEM_SIZE;
+        m_addr.write(addr);
+        wait(m_done.posedge_event());
+
+        checkValue();
+        wait(10, SC_NS);
+    }
+}
+
+
+template<int ADDR_SIZE, int WORD_SIZE, int MEM_SIZE>
+void Top<ADDR_SIZE, WORD_SIZE, MEM_SIZE>::checkValue() const {
+    try {
+        sc_dt::sc_uint<WORD_SIZE> toCompare = m_cachedRam.at(m_addr.read());
+        sc_dt::sc_uint<WORD_SIZE> value = m_val.read();
+        if (value == toCompare) {
+            std::cout << "Got: " << m_addr.read() << " -> " << value << std::endl;
+        }
+        else {
+            std::cerr << "TEST ERROR: Got " << m_addr.read() << " -> " << value <<
+                " instead of " << toCompare << std::endl;
+        }
+    }
+    catch(const std::out_of_range& e) {
+        // Memory was not set ; undefined behavior
+            std::cout << "Got: " << m_addr.read() << " -> <undefined>" << std::endl;
+    }
 }
