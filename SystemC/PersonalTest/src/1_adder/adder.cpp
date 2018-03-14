f85f27d1a4ddc5c0caa3e24df74a421b42b4adf0
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-03-13 22:16:33 -0400

    Made add a method of the Adder module.

--- SystemC/PersonalTest/src/1_adder/adder.cpp
+++ SystemC/PersonalTest/src/1_adder/adder.cpp
@@ -4,9 +4,14 @@
 
 Adder::Adder(::sc_core::sc_module_name name) {
     std::cout << "Constructing adder of name \"" << name << "\"" << std::endl;
+
+    SC_METHOD(add);
+    sensitive << m_a << m_b;
 }
 
 void Adder::add() {
+    std::cout << "Computing a sum" << std::endl;
+
     sc_dt::sc_uint<5> sum = m_a.read() + m_b.read();
     m_sum.write(sum);
 }
