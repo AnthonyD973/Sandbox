4102a97f982efea8d26af920847acf9769f8dc15
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-03-14 13:39:14 -0400

    Added a method 'checkSum' in Top.

--- SystemC/PersonalTest/src/1_adder/top.h
+++ SystemC/PersonalTest/src/1_adder/top.h
@@ -5,14 +5,18 @@ SC_MODULE(Top) {
 
 public:
 
+    sc_core::sc_in< sc_dt::sc_uint<VEC_WIDTH + 1> >             m_sum;
     sc_core::sc_out< sc_dt::sc_uint<VEC_WIDTH> >                m_a;
     sc_core::sc_out< sc_dt::sc_uint<VEC_WIDTH> >                m_b;
 
     SC_CTOR(Top);
 
     void stimulate();
+    void checkSum() const;
 
 };
+
+/////////////////////////////////
 
 template<uint8_t VEC_WIDTH>
 Top<VEC_WIDTH>::Top(::sc_core::sc_module_name name) {
@@ -22,10 +26,23 @@ Top<VEC_WIDTH>::Top(::sc_core::sc_module_name name) {
     
 }
 
+
 template<uint8_t VEC_WIDTH>
 void Top<VEC_WIDTH>::stimulate() {
     while (true) {
         m_a.write(rand());
         m_b.write(rand());
+    }
+}
+
+
+template<uint8_t VEC_WIDTH>
+void Top<VEC_WIDTH>::checkSum() const {
+    sc_dt::sc_uint<VEC_WIDTH + 1> sum = m_a.read() + m_b.read();
+    if (sum != m_sum.read()) {
+        std::cerr << "TEST ERROR: Got " << m_a.read() << " + " << m_b.read() << " = " << m_sum.read() << std::endl;
+    }
+    else {
+        std::cout << "Got " << m_a.read() << " + " << m_b.read() << " = " << m_sum.read() << std::endl;
     }
 }
