1973d6eeeaff4133b39f880e0e1d86fe967cfe4a
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-03-19 21:07:58 -0400

    Added 'read' signal to Top.

--- SystemC/PersonalTest/src/2_ram/sc_main.cpp
+++ SystemC/PersonalTest/src/2_ram/sc_main.cpp
@@ -9,11 +9,13 @@ constexpr int MEM_SIZE  = 65536;
 int sc_main(int argc, char** argv) {
     sc_buffer< sc_dt::sc_uint<ADDR_SIZE> > addr;
     sc_buffer< sc_dt::sc_uint<WORD_SIZE> > val;
+    sc_buffer<bool> read;
     sc_buffer<bool> done;
 
     Top<ADDR_SIZE, WORD_SIZE, MEM_SIZE> top("t1");
     top.m_addr(addr);
     top.m_val(val);
+    top.m_read(read);
     top.m_done(done);
     
     sc_start(200, SC_NS);
--- SystemC/PersonalTest/src/2_ram/top.h
+++ SystemC/PersonalTest/src/2_ram/top.h
@@ -13,6 +13,7 @@ public:
 
     sc_core::sc_in< sc_dt::sc_uint<WORD_SIZE> >                 m_val;
     sc_core::sc_in<bool>                                        m_done;
+    sc_core::sc_out<bool>                                       m_read; // READ : 1 ; WRITE : 0
     sc_core::sc_out< sc_dt::sc_uint<ADDR_SIZE> >                m_addr;
 
     SC_CTOR(Top);
