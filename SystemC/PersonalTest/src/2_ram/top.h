f7050db15adefbfeea5211ed4e5728e4193ff7cf
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-03-22 16:53:46 -0400

    Made use of the new RamValueGenerator class in Top.

--- SystemC/PersonalTest/src/2_ram/top.h
+++ SystemC/PersonalTest/src/2_ram/top.h
@@ -1,32 +1,28 @@
 #include <systemc.h>
 #include <iostream>
-#include <unordered_map>
-#include <cinttypes>
 #include <cstdlib>
 
-#include "sc_uint_hash.h"
+#include "ram_value_generator.h"
 
 template<int ADDR_SIZE, int WORD_SIZE, int MEM_SIZE = 1 << ADDR_SIZE>
 SC_MODULE(Top) {
 
 public:
 
-    sc_core::sc_in< sc_dt::sc_uint<WORD_SIZE> >                 m_val;
+    sc_core::sc_inout< sc_dt::sc_uint<WORD_SIZE> >              m_val;
     sc_core::sc_in<bool>                                        m_done;
     sc_core::sc_out<bool>                                       m_read; // READ : 1 ; WRITE : 0
     sc_core::sc_out< sc_dt::sc_uint<ADDR_SIZE> >                m_addr;
 
     SC_CTOR(Top);
 
-    void stimulateRam();
-    void checkValue() const;
+    void populateRam();
+    void verifyRam();
 
 private:
 
-    std::unordered_map<
-        sc_dt::sc_uint<ADDR_SIZE>,
-        sc_dt::sc_uint<WORD_SIZE>
-    >                                                           m_cachedRam;
+    sc_core::sc_event                                           m_startVerify;
+    RamValueGenerator<ADDR_SIZE, WORD_SIZE>                     m_valueGenerator;
 
 };
 
@@ -36,39 +32,27 @@ template<int ADDR_SIZE, int WORD_SIZE, int MEM_SIZE>
 Top<ADDR_SIZE, WORD_SIZE, MEM_SIZE>::Top(::sc_core::sc_module_name name) {
     std::cout << "Constructing Top module \"" << name << "\"" << std::endl;
 
-    SC_THREAD(stimulateRam);
+    SC_THREAD(populateRam);
+    SC_THREAD(verifyRam);
 
 }
 
 
 template<int ADDR_SIZE, int WORD_SIZE, int MEM_SIZE>
-void Top<ADDR_SIZE, WORD_SIZE, MEM_SIZE>::stimulateRam() {
-    while (true) {
-        int addr = rand() % MEM_SIZE;
+void Top<ADDR_SIZE, WORD_SIZE, MEM_SIZE>::populateRam() {
+    const unsigned int MAX_WRITES = MEM_SIZE / 2;
+    for (unsigned int i = 0; i < rand() % MAX_WRITES; ++i) {
+        sc_dt::sc_uint<ADDR_SIZE> addr = rand();
         m_addr.write(addr);
+        m_val.write(m_valueGenerator.getVal(addr));
         wait(m_done.posedge_event());
-
-        checkValue();
-        wait(10, SC_NS);
     }
 }
 
 
 template<int ADDR_SIZE, int WORD_SIZE, int MEM_SIZE>
-void Top<ADDR_SIZE, WORD_SIZE, MEM_SIZE>::checkValue() const {
-    try {
-        sc_dt::sc_uint<WORD_SIZE> toCompare = m_cachedRam.at(m_addr.read());
-        sc_dt::sc_uint<WORD_SIZE> value = m_val.read();
-        if (value == toCompare) {
-            std::cout << "Got: " << m_addr.read() << " -> " << value << std::endl;
-        }
-        else {
-            std::cerr << "TEST ERROR: Got " << m_addr.read() << " -> " << value <<
-                " instead of " << toCompare << std::endl;
-        }
-    }
-    catch(const std::out_of_range& e) {
-        // Memory was not set ; undefined behavior
-            std::cout << "Got: " << m_addr.read() << " -> <undefined>" << std::endl;
+void Top<ADDR_SIZE, WORD_SIZE, MEM_SIZE>::verifyRam() {
+    while(true) {
+        wait(m_startVerify);
     }
 }
