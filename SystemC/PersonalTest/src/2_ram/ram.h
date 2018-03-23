b940d655cd023eb772b299c7eff24361baf0caa3
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-03-23 18:07:31 -0400

    Added ports to Ram.

--- SystemC/PersonalTest/src/2_ram/ram.h
+++ SystemC/PersonalTest/src/2_ram/ram.h
@@ -5,6 +5,12 @@
 
 template <int ADDR_SIZE, int WORD_SIZE, int MEM_SIZE = 1 << ADDR_SIZE>
 SC_MODULE(Ram) {
+public:
+
+    sc_core::sc_inout< sc_dt::sc_uint<WORD_SIZE> >              m_data;
+    sc_core::sc_in< sc_dt::sc_uint<ADDR_SIZE> >                 m_addr; // READ : 1 ; WRITE : 0
+    sc_core::sc_in<bool>                                        m_read;
+    sc_core::sc_in<bool>                                        m_done;
 
 };
 
