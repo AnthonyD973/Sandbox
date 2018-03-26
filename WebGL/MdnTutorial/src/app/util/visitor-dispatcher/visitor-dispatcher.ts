ddb893f46a7963471243589893ba069a83afc06a
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-03-26 18:39:55 -0400

    Created class VisitorDispatcher.

---
+++ WebGL/MdnTutorial/src/app/util/visitor-dispatcher/visitor-dispatcher.ts
@@ -0,0 +1,24 @@
+import { Visitor } from './visitor';
+import { Visitee } from './visitee';
+
+type Operation<T> = ((v1, v2) => T)[][];
+
+export class VisitorDispatcher
+    <
+        Ret,
+        V1 extends Visitee<number>,
+        V2 extends Visitee<number>,
+        V1Visitor extends Visitor<number, V1>,
+        V2Visitor extends Visitor<number, V2>
+    > {
+
+    private operations: Operation<Ret>[][];
+
+    public VisitorDispatcher(operations: Operation<Ret>[][]) {
+        this.operations = operations;
+    }
+
+    public visit(v1: V1, v2: V2): Ret {
+        return null;
+    }
+}
