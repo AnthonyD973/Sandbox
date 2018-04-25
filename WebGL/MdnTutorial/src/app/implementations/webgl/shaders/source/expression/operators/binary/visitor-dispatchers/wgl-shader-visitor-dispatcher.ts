dd7da1eef9560408119a3b3422ee6320535c67ff
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-04-24 22:10:08 -0400

    Extracted functions from Add VD to the generic Wgl VD

--- WebGL/MdnTutorial/src/app/implementations/webgl/shaders/source/expression/operators/binary/visitor-dispatchers/wgl-shader-visitor-dispatcher.ts
+++ WebGL/MdnTutorial/src/app/implementations/webgl/shaders/source/expression/operators/binary/visitor-dispatchers/wgl-shader-visitor-dispatcher.ts
@@ -3,6 +3,8 @@ import { ShaderExpressionType } from '../../../../../../../../api/shaders/source
 import { WglShaderExpressionTypeVisitor } from '../../../types/wgl-shader-expression-type-visitor';
 import { Visitee } from '../../../../../../../../util/visitor-dispatcher/visitee';
 import { ShaderBinaryOperator } from '../../../../../../../../api/shaders/source/expression/operators/shader-binary-operator';
+import { ShaderMatrixType } from '../../../../../../../../api/shaders/source/expression/types/shader-matrix-type';
+import { ShaderVectorType } from '../../../../../../../../api/shaders/source/expression/types/shader-vector-type';
 
 export abstract class WglShaderVisitorDispatcher
     extends
@@ -26,6 +28,28 @@ export abstract class WglShaderVisitorDispatcher
     // "No". Means the operation on v1 and v2 is invalid.
     public n(v1: ShaderExpressionType, v2: ShaderExpressionType): void {
         throw new Error(`Operation "${this.operationName}" is invalid on given types`);
+    }
+
+    // "Matrix". Asserts whether the matrices' dimensions are equal.
+    protected m(v1: ShaderMatrixType, v2: ShaderMatrixType): void {
+        const isValid = v1.dims.rows === v2.dims.rows && v1.dims.cols === v2.dims.cols;
+        if (!isValid) {
+            this.n(v1, v2);
+        }
+        else {
+            this.y(v1, v2);
+        }
+    }
+
+    // "Vector". Asserts whether the vectors' dimensions are equal.
+    protected v(v1: ShaderVectorType, v2: ShaderVectorType): void {
+        const isValid = v1.dim === v2.dim;
+        if (!isValid) {
+            this.n(v1, v2);
+        }
+        else {
+            this.y(v1, v2);
+        }
     }
 
 }
