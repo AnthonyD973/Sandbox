602004755d74d2347a332aa869b9fe061917523a
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-04-30 18:24:22 -0400

    Created class for add operator.

---
+++ WebGL/MdnTutorial/src/app/implementations/webgl/shaders/source/expression/operators/binary/wgl-shader-add.ts
@@ -0,0 +1,15 @@
+import { ShaderBinaryOperator } from '../../../../../../../api/shaders/source/expression/operators/shader-binary-operator';
+import { ShaderExpression } from '../../../../../../../api/shaders/source/expression/shader-expression';
+import { ShaderExpressionType } from '../../../../../../../api/shaders/source/expression/shader-expression-type';
+
+export class WglShaderAdd implements ShaderBinaryOperator {
+
+    public readonly lhs: ShaderExpression;
+    public readonly rhs: ShaderExpression;
+    public readonly type: ShaderExpressionType;
+
+    public parse(): string {
+        return null;
+    }
+
+}
